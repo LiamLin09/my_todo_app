c8ed5c5e25a14d3ee449ad2077ce76287ad7f524
LiamLin <liamlin5609@gmail.com>
2024-06-03 11:53:22 -0400

    update

--- App1/bonus/bonus17.py
+++ App1/bonus/bonus17.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-camera_image = st.camera_input('Camera')
+with st.expander('start camera'):
+    # start camera
+    camera_image = st.camera_input('Camera')
 
 if camera_image:
     # create a pillow image instance
