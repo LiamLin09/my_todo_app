b8cebd574c2ce3dbd00ca7cb71fa5523b0dee4aa
LiamLin <liamlin5609@gmail.com>
2024-06-05 15:07:49 -0400

    render the app titles

--- App2/main.py
+++ App2/main.py
@@ -1,12 +1,11 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
-
 with col1:
     st.image("images/photo.png")
-
 with col2:
     st.title('Liam Lin')
     content = """
@@ -22,3 +21,14 @@ Feel free to contact me :)
 """
 
 st.write(content2)
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=';')
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+
